8a57c7005b93a2f44118ec490275f2a221839b86
ItsOnlyBinary <git@itsonlybinary.com>
2019-12-06 13:21:55 +0000

    Maybe added temp from gpio but untested

--- octoprint_navbartemp/__init__.py
+++ octoprint_navbartemp/__init__.py
@@ -9,6 +9,7 @@ import octoprint.plugin
 from octoprint.util import RepeatedTimer
 import sys
 import re
+import os
 
 from .libs.sbc import SBCFactory
 
@@ -22,21 +23,23 @@ class NavBarPlugin(octoprint.plugin.StartupPlugin,
         self.piSocTypes = (["BCM2708", "BCM2709",
                             "BCM2835"])  # Array of raspberry pi SoC's to check against, saves having a large if/then statement later
         self.debugMode = False  # to simulate temp on Win/Mac
-        self.displayRaspiTemp = True
+        self.displayTempSoC = True
+        self.displayTempGPIO = True
         self._checkTempTimer = None
         self.sbc = None
 
     def on_after_startup(self):
-        self.displayRaspiTemp = self._settings.get(["displayRaspiTemp"])
+        self.displayTempSoC = self._settings.get(["displayTempSoC"])
+        self.displayTempGPIO = self._settings.get(["displayTempGPIO"])
         self.piSocTypes = self._settings.get(["piSocTypes"])
-        self._logger.debug("displayRaspiTemp: %s" % self.displayRaspiTemp)
+        self._logger.debug("displayTempSoC: %s" % self.displayTempSoC)
 
         if sys.platform == "linux2":
             self.sbc = SBCFactory().factory(self._logger)
 
-            if self.sbc.is_supported and self.displayRaspiTemp:
+            if self.sbc.is_supported and (self.displayTempSoC or self.displayTempGPIO):
                 self._logger.debug("Let's start RepeatedTimer!")
-                self.startTimer(30.0)
+                self.startTimer(10.0)
         # debug mode doesn't work if the OS is linux on a regular pc
         try:
             self._logger.debug("is supported? - %s" % self.sbc.is_supported)
@@ -45,27 +48,61 @@ class NavBarPlugin(octoprint.plugin.StartupPlugin,
 
 
     def startTimer(self, interval):
-        self._checkTempTimer = RepeatedTimer(interval, self.updateSoCTemp, None, None, True)
+        self._checkTempTimer = RepeatedTimer(interval, self.updateTemps, None, None, True)
         self._checkTempTimer.start()
 
-    def updateSoCTemp(self):
-        temp = self.sbc.checkSoCTemp()
-        self._logger.debug("match: %s" % temp)
+    def updateTemps(self):
+        gpio = 0
+        soc = 0
+
+        if self.displayTempGPIO:
+            gpio = self.getTempGPIO()
+        if self.displayTempSoC:
+            soc = self.sbc.checkSoCTemp()
+
+        self._logger.debug("soc: %s" % soc)
         self._plugin_manager.send_plugin_message(self._identifier,
-                                                 dict(isSupported=self.sbc.is_supported,
-                                                      soctemp=temp))
+                                                 dict(isSupported=self.sbc.is_supported,showsoc=self.displayTempSoC,
+                                                      soctemp=soc,showgpio=self.displayTempGPIO,gpiotemp=gpio))
+
+    def getTempGPIO(self):
+        os.system('modprobe w1-gpio')
+        os.system('modprobe w1-therm')
+        base_dir = '/sys/bus/w1/devices/'
+        device_folder = glob.glob(base_dir + '28*')[0]
+        device_file = device_folder + '/w1_slave'
+        if os.path.isfile(device_file):
+            lines = self.readTempGPIO(device_file)
+            count = 5
+            while lines[0].strip()[-3:] != 'YES' and count <= 0:
+                count -= 1
+                time.sleep(0.1)
+                lines = self.readTempGPIO(device_file)
+            equals_pos = lines[1].find('t=')
+            if equals_pos != -1:
+                temp_string = lines[1][equals_pos+2:]
+                temp_c = float(temp_string) / 1000.0
+                p = '{0:0.1f}'.format(temp_c)
+                return p
+        return 'err'
+
+    def readTempGPIO(self, device_file):
+        f = open(device_file, 'r')
+        lines = f.readlines()
+        f.close()
+        return lines
 
     ##~~ SettingsPlugin
     def get_settings_defaults(self):
-        return dict(displayRaspiTemp=self.displayRaspiTemp,
+        return dict(displayTempSoC=self.displayTempSoC,
                     piSocTypes=self.piSocTypes)
 
     def on_settings_save(self, data):
         octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
 
-        self.displayRaspiTemp = self._settings.get(["displayRaspiTemp"])
+        self.displayTempSoC = self._settings.get(["displayTempSoC"])
 
-        if self.displayRaspiTemp:
+        if self.displayTempSoC:
             interval = 5.0 if self.debugMode else 30.0
             self.startTimer(interval)
         else:
